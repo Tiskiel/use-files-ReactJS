a89dc6e69d58646c6be82f58f7fe42c0a451e3f9
tiskiel <wanty.jf@gmail.com>
2022-11-28 17:05:35 +0100

    upgrade

--- src/components/FilesUpload.jsx
+++ src/components/FilesUpload.jsx
@@ -1,29 +1,32 @@
-import {  useState } from 'react'
-import {  useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import * as xlsx from 'xlsx'
 
 
-export default function FilesUpload(props)
-{   
+export default function FilesUpload(props) {
     const [file, setFile] = useState([])
     const [headers, setHeaders] = useState([])
     const [goodHeaders, setGoodHeaders] = useState([])
     const navigate = useNavigate()
-    
+
+    useEffect(() => {
+        if (file.length > 0) navigateToView();
+    }, [file]);
+
     const readUploadFile = (e) => {
         e.preventDefault()
         if (e.target.files) {
 
-            for(let i in e.target.files) {
+            for (let i in e.target.files) {
                 if (isNaN(i)) continue;
-                console.log(i);
                 const file = e.target.files[i];
                 const fileName = file.name
-            
+
+                console.log("BIBLOP")
                 const reader = new FileReader()
                 reader.onload = (e) => {
                     const data = e.target.result
-                    const workbook = xlsx.read(data, { type: "array"})
+                    const workbook = xlsx.read(data, { type: "array" })
                     const sheetName = workbook.SheetNames[0]
                     const worksheet = workbook.Sheets[sheetName]
                     const json = xlsx.utils.sheet_to_json(worksheet)
@@ -31,45 +34,48 @@ export default function FilesUpload(props)
                         name: fileName,
                         file: json
                     }
+                    console.log("BLOP")
                     setFile(current => [...current, newObj])
                 }
-                
 
-            reader.readAsArrayBuffer(file)
+
+                reader.readAsArrayBuffer(file)
             }
-        
+
         }
-            
+
     }
-    
+
     const readUploadHeaderFile = (e) => {
 
-        if(e.target.files)
-        {   
-            
-            for (let i in e.target.files){
-                const fileName = e.target.files[0].name
+        if (e.target.files) {
+
+            for (let i in e.target.files) {
+                console.log(i)
+                if (isNaN(i)) continue;
+                const file = e.target.files[i]
+                const fileName = e.target.files[i].name
                 const reader = new FileReader()
-                
+
                 reader.onload = (e) => {
-                    
+
                     const data = e.target.result
-                    const workbook = xlsx.read(data, { type : 'binary'})
+                    const workbook = xlsx.read(data, { type: 'binary' })
                     const header = getHeader(workbook)
                     setHeaders(current => [...current, ...header])
-                    for ( let i = 0; i < headers.length; i++ ) {
+                    for (let i = 0; i < headers.length; i++) {
                         if (!goodHeaders.includes(headers[i])) {
                             setGoodHeaders(current => [...current, headers[i]])
                         }
                     }
                 }
-                reader.readAsArrayBuffer(e.target.files[0])
+                reader.readAsArrayBuffer(file)
             }
         }
     }
 
     const navigateToView = () => {
-        navigate('/home/upload-files/view-files', { state: {files: file, headers: headers} })
+        navigate('/home/upload-files/view-files', { state: { files: file, headers: headers } })
     }
 
     const onChange = (e) => {
@@ -78,7 +84,7 @@ export default function FilesUpload(props)
         e.target.value = null
     }
 
-    
+
     return (
         <form>
             <label htmlFor="upload">Up file</label>
@@ -98,14 +104,13 @@ export default function FilesUpload(props)
 }
 
 
-function getHeader(workbook)
-{
-    const workbookDone= workbook.Sheets[workbook.SheetNames[0]]
+function getHeader(workbook) {
+    const workbookDone = workbook.Sheets[workbook.SheetNames[0]]
     const header = []
     const columnCount = xlsx.utils.decode_range(workbookDone['!ref']).e.c + 1
 
     for (let i = 0; i < columnCount; i++)
         header[i] = workbookDone[`${xlsx.utils.encode_col(i)}1`].v
-    return header 
+    return header
 }
 
